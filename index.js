c96bd726a24b49960f3c709a36d9af0ff2078b2e
florencialarreategui <florencialarreategui@gmail.com>
2023-01-20 21:06:39 -0300

    maquetado + función canva

---
+++ index.js
@@ -0,0 +1,50 @@
+const botonDibujar = document.querySelector(".boton-dibujar")
+const canvas = document.querySelector("#dibujo");
+const navBar = document.querySelector(".navBar"); 
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const punto = document.createElement("img");
+const flor = document.createElement("img");
+
+punto.src = "./imagenes/dot.png";
+flor.src = "./imagenes/flor7.png";
+
+const context = canvas.getContext("2d");
+
+let pincel = punto;
+
+const handleMouseMove = (event) => {
+   const left = event.clientX;
+   const top = event.clientY;
+   context.drawImage (pincel, left, top)
+};
+
+const handleClick = () => {
+   if (pincel === punto){
+      pincel = flor
+   } else {
+      pincel = punto
+   }
+
+};
+
+botonDibujar.onclick = () => {
+   navBar.classList.add("rojo");
+}
+botonDibujar.onclick = () => {
+   console.log("Funciona")
+ }
+
+// botonDibujar.onclick = () => {
+//    canvas.addEventListener ("click", handleClick) 
+//    canvas.addEventListener ("mousemove", handleMouseMove)  
+  
+
+// }
+
+
+
+
+
